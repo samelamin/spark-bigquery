628539004145a518ce813157ad888ecc6250a713
sam.elamin <sam.elamin@just-eat.com>
2017-02-13 23:20:47 +0000

    improvements

--- src/main/scala/com/samelamin/spark/bigquery/streaming/BigQuerySinkLog.scala
+++ src/main/scala/com/samelamin/spark/bigquery/streaming/BigQuerySinkLog.scala
@@ -8,12 +8,11 @@ import org.apache.spark.sql.functions._
   * Created by sam elamin on 08/02/2017.
   */
 class BigQuerySinkLog(sparkSession: SparkSession, path: String) {
-  private val logPath = new Path(path, "batch_logs.json").toString
 
   def getLatest(): Option[Long] = {
     try {
       import sparkSession.implicits._
-      val df = sparkSession.read.json(logPath).as[Long]
+      val df = sparkSession.read.json(path).as[Long]
       df.show()
       val latest: Long = df
         .sort(desc("inserted_batches"))
@@ -27,6 +26,6 @@ class BigQuerySinkLog(sparkSession: SparkSession, path: String) {
   def writeBatch(batchId: Long):Unit = {
     import sparkSession.implicits._
     val df = Seq(batchId).toDF("inserted_batches").as[Long]
-    df.write.mode(SaveMode.Overwrite).json(logPath)
+    df.write.mode(SaveMode.Overwrite).json(path)
   }
 }
